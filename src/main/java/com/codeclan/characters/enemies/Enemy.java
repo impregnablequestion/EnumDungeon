41871c2e690ff132a17817ee09541b16de2f27be
Séamus <ryans40@tcd.ie>
2023-02-16 11:29:40 +0000

    Gave Enemies ability to Attack

--- src/main/java/com/codeclan/characters/enemies/Enemy.java
+++ src/main/java/com/codeclan/characters/enemies/Enemy.java
@@ -1,17 +1,40 @@
 package com.codeclan.characters.enemies;
 
+import com.codeclan.behaviours.IAttack;
 import com.codeclan.characters.Character;
+import com.codeclan.characters.enums.AttackItem;
+import com.codeclan.characters.enums.WeaponType;
 
-public abstract class Enemy extends Character {
+public abstract class Enemy extends Character implements IAttack {
+
+    protected WeaponType weapon;
 
     public Enemy(String name, int health) {
         super(name, health);
+        this.weapon = WeaponType.SWORD;
     }
 
     @Override
     public boolean receiveHealing(int healing){
         return false;
     }
+
+    @Override
+    public void attack(AttackItem attackItem, Character character) {
+        character.receiveDamage(attackItem.getDamage());
+    }
+
+    @Override
+    public AttackItem getAttackItem() {
+        return weapon;
+    }
+
+    @Override
+    public void changeAttackItem(AttackItem attackItem) {
+        weapon = (WeaponType) attackItem;
+    }
+
+
 
 
 
--- src/main/java/com/codeclan/characters/enemies/Troll.java
+++ src/main/java/com/codeclan/characters/enemies/Troll.java
@@ -1,10 +1,12 @@
 package com.codeclan.characters.enemies;
 import com.codeclan.characters.Character;
+import com.codeclan.characters.enums.WeaponType;
 
 public class Troll extends Enemy{
 
     public Troll(String name, int health) {
         super(name, health);
+        this.weapon = WeaponType.AXE;
     }
 
     public String scream() {
